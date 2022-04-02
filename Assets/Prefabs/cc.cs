80082ee3e08969c2497ba15af303957703bd0f16
Coen Steuns <c.steuns@gmail.com>
2022-04-02 20:49:23 +0200

    this is very sad

---
+++ Assets/Prefabs/cc.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cc : MonoBehaviour
+{
+    private void OnMouseDown() {
+        print("asdf");
+    }
+}
