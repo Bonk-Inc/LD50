29c5683fd10d35523189fdb73b8a97af34175af3
Coen Steuns <c.steuns@gmail.com>
2022-04-02 15:42:33 +0200

    mink-chan animations

--- Assets/Scripts/Player/Movement/PlayerMovement.cs
+++ Assets/Scripts/Player/Movement/PlayerMovement.cs
@@ -4,50 +4,84 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private const string WALKING_ANIMATION_PARAM = "IsWalking";
+
     [SerializeField]
     private float speed = 10f;
 
     private Rigidbody2D rb;
+    private Animator anim;
+    private SpriteRenderer sprite;
 
     private MoveStatus moveStatus;
 
-    private void Start(){
+    private void Start()
+    {
         rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
 
-    private void Update(){
+    private void Update()
+    {
         UpdateMoveStatus();
         Move();
         UpdateVisuals();
     }
 
-    private void UpdateMoveStatus(){
-        if (Input.GetKey(KeyCode.A)){
+    private void UpdateMoveStatus()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
             moveStatus = MoveStatus.LEFT;
-        } else if (Input.GetKey(KeyCode.D)){
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
             moveStatus = MoveStatus.RIGHT;
-        } else {
+        }
+        else
+        {
             moveStatus = MoveStatus.IDLE;
         }
     }
 
-    private void Move(){
+    private void Move()
+    {
         var direction = MoveStatusDirection(); // normalize to 1 and -1
         rb.velocity = new Vector2(direction * speed, rb.velocity.y);
     }
 
-    private float MoveStatusDirection() => moveStatus switch {
+    private float MoveStatusDirection() => moveStatus switch
+    {
         MoveStatus.LEFT => -1,
         MoveStatus.RIGHT => 1,
         _ => 0,
     };
 
-    private void UpdateVisuals(){
-        //TODO handle animation and image rotation
+    private void UpdateVisuals()
+    {
+
+        if (moveStatus == MoveStatus.LEFT)
+        {
+            anim.SetBool("IsWalking", true);
+            sprite.flipX = true;
+        }
+        else if (moveStatus == MoveStatus.RIGHT)
+        {
+            anim.SetBool("IsWalking", true);
+            sprite.flipX = false;
+        }
+        else
+        {
+            anim.SetBool("IsWalking", false);
+            sprite.flipX = false;
+
+        }
     }
 }
 
-public enum MoveStatus {
+public enum MoveStatus
+{
     LEFT,
     RIGHT,
     IDLE
