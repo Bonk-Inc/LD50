ad8c7ddf748f01f03445d9c1a7a4c0808c55f95c
Traxx186 <justinvanderkruit@icloud.com>
2022-04-02 17:18:29 +0200

    Created separate wagon class

---
+++ Assets/Scripts/Wagon.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wagon : MonoBehaviour
+{
+    [SerializeField]
+    private List<GameObject> parts;
+
+    public List<GameObject> Parts => parts;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+}
