fc34b5c962713d37bc5acc86b454ef370db194a9
Traxx186 <justinvanderkruit@icloud.com>
2022-04-03 18:18:53 +0200

    Merge branch 'main' into feature/traxxy-chan

--- Assets/Scripts/Utils/Timer.cs
+++ Assets/Scripts/Utils/Timer.cs
@@ -2,37 +2,51 @@ using System.Collections;
 using System;
 using UnityEngine;
 
-public class Timer : MonoBehaviour {
-    
-    [SerializeField]
+public class Timer : MonoBehaviour
+{
+    [SerializeField] 
     private float timeOfSecond = 1;
+
+    [SerializeField] 
+    private GameObject gameOverPrefab;
+
     private Coroutine timer;
+
     private float timeSurvived = 0;
+    
     public float TimeSurvived => timeSurvived;
 
-    public event Action<float> OnTimeUpdate;
-    
-    private void Start() {
+    public event Action<float> OnGameFinish;
+
+    private void Start()
+    {
         StartTimer();
     }
 
-    private IEnumerator TimerRoutine() {
-        while (true) {
+    private IEnumerator TimerRoutine()
+    {
+        while (true)
+        {
             yield return new WaitForSeconds(timeOfSecond);
             timeSurvived += timeOfSecond;
-            OnTimeUpdate?.Invoke(timeSurvived);
         }
     }
 
-    public void ResetTimeSurvived() {
+    public void ResetTimeSurvived()
+    {
         timeSurvived = 0;
     }
 
-    public void StartTimer() {
+    public void StartTimer()
+    {
         timer = StartCoroutine("TimerRoutine");
     }
 
-    public void StopTimer() {
+    public void StopTimer()
+    {
         StopCoroutine(timer);
+        
+        gameOverPrefab.SetActive(true);
+        OnGameFinish?.Invoke(timeSurvived);
     }
 }
