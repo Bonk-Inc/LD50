8ce6b35fd0f1833ea0fa38d98c311326a3800a8d
jvdwijk <joeyvdwijk@gmail.com>
2022-04-03 13:33:00 +0200

    complete furni

--- Assets/Scripts/Minigames/Furnace/FuelingFurnace.cs
+++ Assets/Scripts/Minigames/Furnace/FuelingFurnace.cs
@@ -5,12 +5,16 @@ using UnityEngine;
 public class FuelingFurnace : MonoBehaviour
 {
     [SerializeField]
+    private MinigameStatus status;
+
+    [SerializeField]
     private CoalCatcher cather;
     private void Awake() {
         cather.OnCoalCatched += FuelFurnace;
     }
 
     private void FuelFurnace() {
-        //TODO Should fill furnace amount when minigame type is set to active. Currently completes the minigame
+        //TODO Should fill furnace amount when minigame type is set to active. Currently completes the minigame\
+        status.CompleteMinigame();
     }
 }
