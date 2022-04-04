1fe9962c10670700505012596631d9579a3a7805
Traxx186 <justinvanderkruit@icloud.com>
2022-04-05 01:54:03 +0200

    Added soundeffects

--- Assets/Scripts/Minigames/Bar/Ingredient.cs
+++ Assets/Scripts/Minigames/Bar/Ingredient.cs
@@ -6,12 +6,16 @@ public class Ingredient : MonoBehaviour
     [SerializeField] 
     private string ingredientName;
 
+    [SerializeField]
+    private PlaySound playSound;
+    
     public event Action<Ingredient> OnIngredientClick;
 
     public string getIngredientName => ingredientName;
     
     private void OnMouseDown()
     {
+        playSound.PlayClip();
         OnIngredientClick?.Invoke(gameObject.GetComponent<Ingredient>());
     }
 }
