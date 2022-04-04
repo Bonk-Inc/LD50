1fa371ac35bac380e75b52f3f1c0aab7b0817e28
Coen Steuns <c.steuns@gmail.com>
2022-04-04 23:04:20 +0200

    guitar minigame

---
+++ Assets/Scripts/Minigames/Guitar/GuitarString.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuitarString : MonoBehaviour
+{
+    
+    [SerializeField]
+    private AudioSource audioSource;
+
+    [SerializeField]
+    private SpriteRenderer StringRenderer;
+
+    [SerializeField]
+    private float highlightTimeInSeconds = 0.5f;
+
+    [SerializeField]
+    private float clickedTimeInSeconds = 0.2f;
+    private bool isLocked = false;
+
+    private Coroutine clickedRoutine;
+
+    public event Action StringClicked;
+
+    public Coroutine Highlight(){
+        return StartCoroutine(HiglightRoutine());
+    }
+
+    private IEnumerator HiglightRoutine(){
+        StringRenderer.color = Color.green;
+        audioSource.Play();
+        yield return new WaitForSeconds(highlightTimeInSeconds);
+        StringRenderer.color = Color.black;
+    }
+
+    public Coroutine Wrong(float time){
+        return StartCoroutine(WrongRoutine(time));
+    }
+
+    private IEnumerator WrongRoutine(float time){
+        isLocked = true;
+        StringRenderer.color = Color.red;
+        yield return new WaitForSeconds(time);
+        StringRenderer.color = Color.black;
+        isLocked = false;
+    }
+
+    private IEnumerator ClickedRoutine(){
+        StringRenderer.color = Color.blue;
+        yield return new WaitForSeconds(clickedTimeInSeconds);
+        StringRenderer.color = Color.black;
+    }
+
+    public void SetLocked(bool locked){
+        isLocked = locked;
+    }
+
+    private void OnMouseUpAsButton() {
+        if(isLocked)
+            return;
+
+        if (clickedRoutine != null)
+        {
+            StopCoroutine(clickedRoutine);
+        }
+
+        clickedRoutine = StartCoroutine(ClickedRoutine());
+        audioSource.Play();
+        StringClicked?.Invoke();
+    }
+    
+
+}
